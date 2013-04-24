8b3d892e8a1aa41bcec95458bc6cfc860ab6258b
Ghostcandywyj <ghost.wyj@gmail.com>
2013-04-24 13:58:17 +0800

    Signed-off-by: Ghostcandywyj <ghost.wyj@gmail.com>

--- sys/core/init.inc.php
+++ sys/core/init.inc.php
@@ -10,7 +10,7 @@ foreach( $Const as $name => $val ){
 }
 
 function __autoload($class_name){
-	$filename = $_SERVER['DOCUMENT_ROOT']."/Nju_Volunteer/sys/class/class.".$class_name.".inc.php";
+	$filename = $_SERVER['DOCUMENT_ROOT']."/NjuVolunteer/sys/class/class.".$class_name.".inc.php";
 	if( file_exists($filename) ){
 		include_once( $filename );
 	}
