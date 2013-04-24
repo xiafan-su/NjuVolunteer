3b12c49c74c0b963132ea0d8e7627ad11ac233a6
Demonsu <245681117@qq.com>
2013-04-24 08:49:09 +0800

    Signed-off-by: Demonsu <245681117@qq.com>

--- public/handle/act.php
+++ public/handle/act.php
@@ -3,12 +3,6 @@ $_BASE_PATH = "../../";
 $_SMARTY_ROOT="../tpls";
 include_once '../../sys/core/init.inc.php';
 
-function utf8Substr($str, $from, $len)
-{
-    return preg_replace('#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$from.'}'.
-                       '((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$len.'}).*#s',
-                       '$1',$str);
-}
 
 $tpl->assign("act_list",$act_listinfo);
 $tpl->display("handle/act_handle.html");
