a8b802dc6b8d930bd79a7493a6cb4da73e8d00a5
Demonsu <245681117@qq.com>
2013-04-24 08:42:55 +0800

    Update act.php

--- public/act.php
+++ public/act.php
@@ -2,17 +2,7 @@
 
 include_once '../sys/core/init.inc.php';
 include './include/header.php';
-		//$act =new Act();
-		//$select=$act->fetch_all("",0,5);
-		$root_conn = mysql_pconnect('localhost', 'root', '920328') or trigger_error(mysql_error(),E_USER_ERROR); 
-		mysql_select_db('nju_volunteer', $root_conn);
-		mysql_query("set names utf8");
-		$query="select * from activity_info";
-		$select=mysql_query($query,$root_conn)or trigger_error(mysql_error(),E_USER_ERROR);
 
-while($act_info = mysql_fetch_assoc($select)){
-	$act_listinfo[] = array('name' => $act_info['name'],'state' => $act_info['state'],'profile' => $act_info['profile'],'time' => $act_info['begin_time'],'place' => $act_info['place'],'offer_num' => $act_info['offer_num']);
-}
 
 $tpl->assign("act_list",$act_listinfo);
 
