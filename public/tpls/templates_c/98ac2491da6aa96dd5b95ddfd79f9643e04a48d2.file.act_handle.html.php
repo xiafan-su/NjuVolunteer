86dcae0c540decb3d8116f14ef33445db86bf612
Demonsu <245681117@qq.com>
2013-04-24 08:58:53 +0800

    Signed-off-by: Demonsu <245681117@qq.com>

---
+++ public/tpls/templates_c/98ac2491da6aa96dd5b95ddfd79f9643e04a48d2.file.act_handle.html.php
@@ -0,0 +1,55 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2013-04-24 08:55:00
+         compiled from "../tpls/templates\handle\act_handle.html" */ ?>
+<?php /*%%SmartyHeaderCode:249651772d64a01d92-29562757%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '98ac2491da6aa96dd5b95ddfd79f9643e04a48d2' => 
+    array (
+      0 => '../tpls/templates\\handle\\act_handle.html',
+      1 => 1366727674,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '249651772d64a01d92-29562757',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'act_list' => 0,
+    'act' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_51772d64a666f',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51772d64a666f')) {function content_51772d64a666f($_smarty_tpl) {?><?php  $_smarty_tpl->tpl_vars['act'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['act']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['act_list']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['act']->key => $_smarty_tpl->tpl_vars['act']->value){
+$_smarty_tpl->tpl_vars['act']->_loop = true;
+?>
+<div class="summary">
+    <div class="act_img">
+        <img src="./include/img/activity/.1jpg" width="200px" height="130px"href="www.baidu.com"/>
+    </div>
+    <div class="words">
+        <span>
+            <h2><a href="activity/detail.php?activityId=1" target="_blank"><?php echo $_smarty_tpl->tpl_vars['act']->value['name'];?>
+</a><span class="atc_state"><?php echo $_smarty_tpl->tpl_vars['act']->value['state'];?>
+</span></h2>
+            <p><?php echo $_smarty_tpl->tpl_vars['act']->value['profile'];?>
+</p>
+        </span>
+        <span class="otherInfo">
+            <span class="first">活动时间：<?php echo $_smarty_tpl->tpl_vars['act']->value['time'];?>
+</span>
+            <span>活动地点：<?php echo $_smarty_tpl->tpl_vars['act']->value['place'];?>
+</span>
+            <span>招募人数：<em><?php echo $_smarty_tpl->tpl_vars['act']->value['offer_num'];?>
+</em></span>
+        </span>
+    </div>
+</div>
+<?php } ?>
+<?php }} ?>
--- public/tpls/templates_c/a82f01f400a1d4be5d0cf3f5fa9b1056671e35a8.file.index.html.php
+++ public/tpls/templates_c/a82f01f400a1d4be5d0cf3f5fa9b1056671e35a8.file.index.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2013-04-23 18:56:32
+<?php /* Smarty version Smarty-3.1.7, created on 2013-04-24 08:54:39
          compiled from "./tpls/templates\index.html" */ ?>
 <?php /*%%SmartyHeaderCode:28950516a173f951b03-87385774%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'a82f01f400a1d4be5d0cf3f5fa9b1056671e35a8' => 
     array (
       0 => './tpls/templates\\index.html',
-      1 => 1366714586,
+      1 => 1366763882,
       2 => 'file',
     ),
   ),
