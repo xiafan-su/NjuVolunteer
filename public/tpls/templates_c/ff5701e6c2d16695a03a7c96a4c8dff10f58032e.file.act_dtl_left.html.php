8c2b363025827e0f76fd52ba73c5a775be85a10b
Ghostcandywyj <ghost.wyj@gmail.com>
2013-05-16 23:08:21 +0800

    Signed-off-by: Ghostcandywyj <ghost.wyj@gmail.com>

--- public/tpls/templates_c/ff5701e6c2d16695a03a7c96a4c8dff10f58032e.file.act_dtl_left.html.php
+++ public/tpls/templates_c/ff5701e6c2d16695a03a7c96a4c8dff10f58032e.file.act_dtl_left.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2013-05-16 11:27:54
+<?php /* Smarty version Smarty-3.1.7, created on 2013-05-16 22:48:26
          compiled from "./tpls/templates\include\act_dtl_left.html" */ ?>
 <?php /*%%SmartyHeaderCode:323775194523ab2cad4-16299542%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ff5701e6c2d16695a03a7c96a4c8dff10f58032e' => 
     array (
       0 => './tpls/templates\\include\\act_dtl_left.html',
-      1 => 1368538893,
+      1 => 1368687260,
       2 => 'file',
     ),
   ),
@@ -15,13 +15,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.7',
   'unifunc' => 'content_5194523ab314b',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5194523ab314b')) {function content_5194523ab314b($_smarty_tpl) {?><!--script src="./assets/js/act_dtl.js" ></script-->
 <div id="act_image">
-    <img src="./assets/img/activity_center/0.jpg" height="300" width="180" />
+    <img src="./assets/img/activity_center/0.jpg" height="300" width="220" />
 </div>
 
 <div id="option">
