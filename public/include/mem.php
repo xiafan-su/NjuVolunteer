cd63d6373114d667207214b1036e15e4bef211fc
yeguangsuixing <411707420@qq.com>
2013-05-17 12:53:57 +0800

    Signed-off-by: yeguangsuixing <411707420@qq.com>

--- public/include/mem.php
+++ public/include/mem.php
@@ -5,6 +5,20 @@ $_SMARTY_ROOT = "../tpls";
 include_once '../../sys/core/init.inc.php';
 
 
+$mem_list = array();
+$team = new Team();
+$mem_info = $team->fetch_my_fellows();
+while( $mem_row = mysql_fetch_array( $mem_info ) ){
+	$mem_list[] = array(
+			"id"=>$mem_row['id'],
+			"name"=>$mem_row['name'],
+			"gender"=>$mem_row['gender'],
+			"tel"=>$mem_row['phone'],
+			"state"=>"没给我",
+			"q"=>"主席",
+	);
+}
+$tpl->assign( "mem_list", $mem_list );
 
 $tpl->display( "include/mem.html" );
 
--- public/include/note_edit.php
+++ public/include/note_edit.php
@@ -4,6 +4,46 @@ $_BASE_PATH = "../../";
 $_SMARTY_ROOT = "../tpls";
 include_once '../../sys/core/init.inc.php';
 
+
+if( isset( $_POST['noteId'] ) ){
+	//从数据库获取并显示
+}
+
+
+
+$team = new Team();
+//院系组织
+$team_list = array();
+$team_info = $team->fetch_all_team();
+while( $team_row = mysql_fetch_array( $team_info ) ){
+	$team_list[] = array(
+		"id" => 	$team_row['id'],
+		"name" => 	$team_row['name'],
+	);
+}
+$tpl->assign( "team_list", $team_list );
+//活动列表
+$act_list = array();
+$act_info = $team->fetch_act_all( $_SESSION[User::USER][User::FACULTY_ID], 1 );//1表示发起的活动
+while( $act_row = mysql_fetch_array( $act_info ) ){
+	$act_list[] = array(
+		"id" => 	$act_row['id'],
+		"name" => 	$act_row['name'],//."测试一下长长的标题测试一下长长的标题测试一下长长的标题"
+	);
+}
+$tpl->assign( "act_list", $act_list );
+//成员
+$mem_list = array();
+$mem_info = $team->fetch_my_fellows();
+while( $mem_row = mysql_fetch_array( $mem_info ) ){
+	$mem_list[] = array(
+		"id" => 	$mem_row['id'],
+		"name" => 	$mem_row['name'],
+	);
+}
+$tpl->assign( "mem_list", $mem_list );
+
+
 $tpl->display( "include/note_edit.html" );
 
 
