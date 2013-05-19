c7ab3f919f9b2cf97252890037fc6d8ad9ebf6a9
Aresxiao <xiaogeng91@foxmail.com>
2013-05-20 04:14:10 +0800

    Signed-off-by: Aresxiao <xiaogeng91@foxmail.com>password md5

--- public/tpls/templates_c/ca42c2d9adf86cfe0b6f0dde3518a36e842461f6.file.super_admin_add_team.html.php
+++ public/tpls/templates_c/ca42c2d9adf86cfe0b6f0dde3518a36e842461f6.file.super_admin_add_team.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2013-05-20 03:10:30
+<?php /* Smarty version Smarty-3.1.7, created on 2013-05-20 04:12:22
          compiled from "../tpls/templates\handle\super_admin_add_team.html" */ ?>
 <?php /*%%SmartyHeaderCode:2769151990f21f24078-63970665%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ca42c2d9adf86cfe0b6f0dde3518a36e842461f6' => 
     array (
       0 => '../tpls/templates\\handle\\super_admin_add_team.html',
-      1 => 1368988375,
+      1 => 1368994292,
       2 => 'file',
     ),
   ),
@@ -28,14 +28,14 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 					<th>填写值</th>
                     </tr>
                
-
+<script src="./assets/js/md5.js"></script>
 <form action="../public/handle/super_admin_add_team_ok.php" method="POST">
 <tr><td style="text-align:left">团队账号名</td>
 <td><input type="text" name="teamid"></td></tr>
 <tr><td style="text-align:left">团队名称</td>
 <td><input type="text" name="teamname"></td></tr>
 <tr><td style="text-align:left">团队密码</td>
-<td><input type="password" name="password"></td></tr>
+<td><input type="password" name="password" id="password"></td></tr>
 <tr><td style="text-align:left">团队联系邮箱</td>
 <td><input type="text" name="teamemail"></td></tr>
 <tr><td style="text-align:left">团队负责人姓名</td>
@@ -50,7 +50,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <td><input type="text" name="teamunit"></td></tr>
 
 
- <tr><td style="text-align:left">请确认</td><td><input type="submit" value="确认创建" /></td>
+ <tr><td style="text-align:left">请确认</td><td><input type="submit" value="确认创建" onclick="$('#password').val(hex_md5($('#password').val()));"/></td>
 </form>
 
 </table>
