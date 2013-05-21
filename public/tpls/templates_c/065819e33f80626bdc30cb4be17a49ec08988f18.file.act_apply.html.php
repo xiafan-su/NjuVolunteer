d6814542f029fa028d9c43b4c26032afa1cbe853
Aresxiao <xiaogeng91@foxmail.com>
2013-05-21 22:15:50 +0800

    Signed-off-by: Aresxiao <xiaogeng91@foxmail.com>modified checkjs, when come across a error ,can display with different color

--- public/tpls/templates_c/065819e33f80626bdc30cb4be17a49ec08988f18.file.act_apply.html.php
+++ public/tpls/templates_c/065819e33f80626bdc30cb4be17a49ec08988f18.file.act_apply.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2013-05-20 05:43:03
+<?php /* Smarty version Smarty-3.1.7, created on 2013-05-21 21:59:40
          compiled from "./tpls/templates\act_apply.html" */ ?>
 <?php /*%%SmartyHeaderCode:238135199476742c1a3-00395143%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '065819e33f80626bdc30cb4be17a49ec08988f18' => 
     array (
       0 => './tpls/templates\\act_apply.html',
-      1 => 1368980531,
+      1 => 1369144567,
       2 => 'file',
     ),
   ),
@@ -15,6 +15,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5199476755240',
   'variables' => 
   array (
     'faculty_list' => 0,
@@ -22,14 +24,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'act_id' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.7',
-  'unifunc' => 'content_5199476755240',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5199476755240')) {function content_5199476755240($_smarty_tpl) {?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="SHORTCUT ICON" href="./assets/img/header/logo.png">
 <link href="./assets/css/main.css" rel="stylesheet" />
 
 
@@ -55,8 +56,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <body>
 <?php echo $_smarty_tpl->getSubTemplate ("include/header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
+
 <div class="main">
-		<div class="span_left">	
+		<div class="span_left">
 				<div class="breadcrumb">
                 	<div class="bar-left"></div>
                     <div class="bar-title">活动资料</div>
@@ -305,8 +307,6 @@ $_smarty_tpl->tpl_vars['faculty']->_loop = true;
 				</div>
 		</div>
 
-		<div class="span_right" id="span_right">
-		</div>
  
 </div>
 <?php echo $_smarty_tpl->getSubTemplate ("include/footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
---
+++ public/tpls/templates_c/b50b7562c6d0bda8d5967293bd62cebc8b3648b0.file.footer.html.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2013-05-21 21:59:41
+         compiled from "./tpls/templates\include\footer.html" */ ?>
+<?php /*%%SmartyHeaderCode:4964519b7dcd658301-19164888%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'b50b7562c6d0bda8d5967293bd62cebc8b3648b0' => 
+    array (
+      0 => './tpls/templates\\include\\footer.html',
+      1 => 1368769060,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '4964519b7dcd658301-19164888',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_519b7dcd669c3',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_519b7dcd669c3')) {function content_519b7dcd669c3($_smarty_tpl) {?><div class="footer">
+	<div class="copyright">
+    Copyright©2013 VAHStudio,All Rights Reserved </br>
+     使用过程中有任何问题请联系我们，邮箱:VAHStudio@126.com
+     </div>
+</div>
+<?php }} ?>
